7f6667ff774b88fac05256aabb0081cc9ab0e4bf
Amirali <amiralihabibi82@gmail.com>
2024-10-12 20:02:48 +0330

    Update the playercards info

--- src/components/Home/meetPlayers/cards.js
+++ src/components/Home/meetPlayers/cards.js
@@ -4,50 +4,73 @@ import Otamendi from '../../../Resources/images/players/Otamendi.png';
 import Sterling from '../../../Resources/images/players/Raheem_Sterling.png';
 import Kompany from '../../../Resources/images/players/Vincent_Kompany.png';
 import PlayerCard from '../../Utils/playerCard';
+import { cityDB } from '../../../temp/m-city-export';
 
 let cards = [
     {
         bottom: 90,
         left: 300,
-        player: Kompany
+         playerImg: Sterling,
+        playerName: 'Raheem'
     },
     {
         bottom: 60,
         left: 200,
-        player: Sterling
+        playerImg: Otamendi,
+        playerName: 'Nicolas'
     },
     {
         bottom: 30,
         left: 100,
-        player: Otamendi
+          playerImg: Kompany,
+        playerName: 'Vincent'
     },
     {
         bottom: 0,
         left: 0,
-        player: Kompany
+        playerImg: Otamendi,
+        playerName: 'Nicolas'
     }
 ];
 
 const HomeCards = (props) => {
 
     const showAnimateCards = () => (
-        cards.map((card, i) => (
-            <motion.div
-                key={i}
-                initial={{ left: 0, bottom: 0 }} // Start position
-                animate={{ left: card.left, bottom: card.bottom }} // Target position
-                transition={{ delay: 0.5, duration: 0.5, ease: "easeOut" }} // Animation config
-                style={{ position: 'absolute' }} // Maintain absolute positioning
-            >
-                <PlayerCard
-                    number="30"
-                    name="Nicolas"
-                    lastname="Otamendi"
-                    bck={card.player}
-                />
-            </motion.div>
-        ))
+        cards.map((card, i) => {
+            const foundPlayer = findPlayer(card.playerName);
+            console.log(foundPlayer);
+            return (
+
+                <motion.div
+                    key={i}
+                    initial={{ left: 0, bottom: 0 }} // Start position
+                    animate={{ left: card.left, bottom: card.bottom }} // Target position
+                    transition={{ delay: 0.5, duration: 0.5, ease: "easeOut" }} // Animation config
+                    style={{ position: 'absolute' }} // Maintain absolute positioning
+                >
+
+                    <PlayerCard
+                        number={foundPlayer.number}
+                        name={foundPlayer.name}
+                        lastname={foundPlayer.lastname}
+                        bck={card.playerImg}
+                    />
+                </motion.div>
+            )
+        })
     );
+
+    const findPlayer = (playerName) => {
+        console.log(playerName);
+        return cityDB.players.find((player) => {
+            if (playerName.toLowerCase() === player.name.toLowerCase()) {
+                console.log(player);
+                console.log("found");
+                return true; // This will cause `find` to return the matched player
+            }
+            return false;
+        });
+    }
 
     return (
         <div>
