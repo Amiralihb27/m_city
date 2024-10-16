7cc30dcb3e23bccf5e0bdde6e385222b2e55b1f0
Amirali <amiralihabibi82@gmail.com>
2024-10-16 21:56:17 +0330

    corrected the type property of customTextField

--- src/components/Admin/players/customTextField.js
+++ src/components/Admin/players/customTextField.js
@@ -3,20 +3,21 @@ import { FormControl, TextField } from '@mui/material';
 import { textErrorHelper, selectErrorHelper, selectIsError } from "../../Utils/tools";
 
 
-const CustomTextField = ({ id, name, placeholder, formik }) => {
+const CustomTextField = ({ id, name, type = 'text', placeholder, formik }) => {
     return (
         <div className="mb-5">
             <FormControl>
                 <TextField
                     id={id}
+                    type={type}
                     name={name}
                     variant="outlined"
                     placeholder={placeholder}
                     {...formik.getFieldProps(name)}
                     {...textErrorHelper(formik, name)}
-                    //     {textErrorHelper(formik, name)} would pass the entire object as a single prop (which TextField wouldn't understand), like:
-                    // <TextField errorHelper={{ error: true, helperText: "some error" }} />
-                    // This won't work because TextField expects error and helperText to be separate props, not wrapped inside another object.
+                //     {textErrorHelper(formik, name)} would pass the entire object as a single prop (which TextField wouldn't understand), like:
+                // <TextField errorHelper={{ error: true, helperText: "some error" }} />
+                // This won't work because TextField expects error and helperText to be separate props, not wrapped inside another object.
 
 
                 //     {...textErrorHelper(formik, name)} spreads the object's properties (i.e., error and helperText) directly into TextField, like:
